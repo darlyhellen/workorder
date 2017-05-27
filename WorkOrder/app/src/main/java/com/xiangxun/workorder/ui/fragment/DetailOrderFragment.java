56876f0b2de27f694c090c1c609bb907a1555d6c
STOCompany <598596582@qq.com>
2017-05-27 16:56:04 +0800

    2017.05.27日更新内容如下：          {              @Time 16:50  编辑工单详情页面          }

--- WorkOrder/app/src/main/java/com/xiangxun/workorder/ui/fragment/DetailOrderFragment.java
+++ WorkOrder/app/src/main/java/com/xiangxun/workorder/ui/fragment/DetailOrderFragment.java
@@ -1,23 +1,17 @@
 package com.xiangxun.workorder.ui.fragment;
 
-import android.app.DialogFragment;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
 
 import com.hellen.baseframe.binder.InitBinder;
-import com.hellen.baseframe.common.dlog.DLog;
+import com.hellen.baseframe.common.obsinfo.ToastApp;
 import com.xiangxun.workorder.R;
-import com.xiangxun.workorder.base.AppEnum;
-import com.xiangxun.workorder.widget.header.HeaderView;
+import com.xiangxun.workorder.bean.WorkOrderData;
 
 /**
  * Created by Zhangyuhui/Darly on 2017/5/27.
@@ -28,6 +22,31 @@ import com.xiangxun.workorder.widget.header.HeaderView;
  */
 public class DetailOrderFragment extends Fragment {
     private View root;
+
+    private WorkOrderData data;
+
+    private TextView tvContent01;
+    private TextView tvContent02;
+    private TextView tvContent03;
+    private TextView tvContent04;
+    private TextView tvContent05;
+    private TextView tvContent06;
+    private TextView tvContent07;
+    private TextView tvContent08;
+    private TextView tvContent09;
+    private TextView tvContent10;
+    private TextView tvContent11;
+    private TextView tvContent12;
+    private TextView tvContent13;
+    private TextView tvContent14;
+    private TextView tvContent15;
+    private TextView tvContent16;
+    private TextView tvContent17;
+    private TextView tvContent18;
+    private TextView tvContent19;
+    private TextView tvContent20;
+    private TextView tvContent21;
+    private TextView tvContent22;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -45,7 +64,59 @@ public class DetailOrderFragment extends Fragment {
 
 
     private void initView() {
+        data = (WorkOrderData) getActivity().getIntent().getSerializableExtra("data");
+        if (data == null) {
+            ToastApp.showToast("页面数据错误");
+            return;
+        }
+        tvContent01 = (TextView) root.findViewById(R.id.tv_content01);
+        tvContent02 = (TextView) root.findViewById(R.id.tv_content02);
+        tvContent03 = (TextView) root.findViewById(R.id.tv_content03);
+        tvContent04 = (TextView) root.findViewById(R.id.tv_content04);
+        tvContent05 = (TextView) root.findViewById(R.id.tv_content05);
+        tvContent06 = (TextView) root.findViewById(R.id.tv_content06);
+        tvContent07 = (TextView) root.findViewById(R.id.tv_content07);
+        tvContent08 = (TextView) root.findViewById(R.id.tv_content08);
+        tvContent09 = (TextView) root.findViewById(R.id.tv_content09);
+        tvContent10 = (TextView) root.findViewById(R.id.tv_content10);
+        tvContent11 = (TextView) root.findViewById(R.id.tv_content11);
+        tvContent12 = (TextView) root.findViewById(R.id.tv_content12);
+        tvContent13 = (TextView) root.findViewById(R.id.tv_content13);
+        tvContent14 = (TextView) root.findViewById(R.id.tv_content14);
+        tvContent15 = (TextView) root.findViewById(R.id.tv_content15);
+        tvContent16 = (TextView) root.findViewById(R.id.tv_content16);
+        tvContent17 = (TextView) root.findViewById(R.id.tv_content17);
+        tvContent18 = (TextView) root.findViewById(R.id.tv_content18);
+        tvContent19 = (TextView) root.findViewById(R.id.tv_content19);
+        tvContent20 = (TextView) root.findViewById(R.id.tv_content20);
+        tvContent21 = (TextView) root.findViewById(R.id.tv_content21);
+        tvContent22 = (TextView) root.findViewById(R.id.tv_content22);
+        initData();
+    }
 
+    private void initData() {
+        tvContent01.setText(data.id);
+        tvContent02.setText(data.devicename);
+        tvContent03.setText(data.offtime);
+        tvContent04.setText(data.telephone);
+        tvContent05.setText(data.devicename);
+        tvContent06.setText(data.status);
+        tvContent07.setText(data.devicename);
+        tvContent08.setText(data.companyid);
+        tvContent09.setText(data.devicename);
+        tvContent10.setText(data.position);
+        tvContent11.setText(data.status);
+        tvContent12.setText(data.messages);
+        tvContent13.setText(data.devicename);
+        tvContent14.setText(data.devicename);
+        tvContent15.setText(data.devicename);
+        tvContent16.setText(data.devicename);
+        tvContent17.setText(data.devicename);
+        tvContent18.setText(data.devicename);
+        tvContent19.setText(data.devicename);
+        tvContent20.setText(data.devicename);
+        tvContent21.setText(data.devicename);
+        tvContent22.setText(data.assigntime);
     }
 
     private void initListener() {
